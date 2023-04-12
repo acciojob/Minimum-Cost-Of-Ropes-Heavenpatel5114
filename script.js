5d74bf9121feb78dd2813d33ccca0183a89fdb58
Heavenpatel5114 <113304844+Heavenpatel5114@users.noreply.github.com>
2023-04-12 10:02:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
-class Solution
-{
-    public:
-    //Function to return the minimum cost of connecting the ropes.
-    long long minCost(long long arr[], long long n) {
-        // Your code here
-        multiset<long long> ms;
-        for(int i=0;i<n;i++) {
-            ms.insert(arr[i]);
-        }
-        long long ans=0;
-        while(ms.size() > 1) {
-            auto it = ms.begin();
-            long long cost = *it;
-            ms.erase(it);
-            it = ms.begin();
-            cost += *it;
-            ms.erase(it);
-            ms.insert(cost);
-            ans += cost;
-        }
-        return ans;
-    }
-};
+function calculateMinCost() {
+  //your code here
+  
+  
+  
+}  
