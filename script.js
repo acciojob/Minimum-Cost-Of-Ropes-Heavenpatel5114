a8eb48817a1ec3f25f7ecce677225231fb65d123
Heavenpatel5114 <113304844+Heavenpatel5114@users.noreply.github.com>
2023-05-01 01:03:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,23 @@
 function calculateMinCost() {
-//your code here
-const str = document.querySelector('#rope-lengths').value
-const strArr = str.split(',')
-const arr = strArr.map(e => {
-return Number.parseInt(e);
-})
-let n = arr.length
+  //your code here
+  let inp=document.getElementById("rope-lengths").value;
+	let arr=inp.split(" ");
+	var sum=0;
+	while(arr.length!=1)
+		{
+			arr.sort((a,b)=>{return a-b;})
+			
+			let a=parseInt(arr.shift());
+			let b=parseInt(arr.shift());
+			let temp=a+b;
+			sum+=temp;
+			arr.push(temp);
+			
+		}
 
-let ans = helper(arr, n)
-document.querySelector('#result').innerText = ans;
-}
+	let res=document.getElementById('result');
 
-function helper(arr, n) {
-// Create a priority queue
-let pq = [];
-
-// Adding items to the pQueue
-for (let i = 0; i < n; i++) {
-pq.push(arr[i]);
-}
-
-pq.sort(function(a,b){return a-b;});
-
-// Initialize result
-let res = 0;
-
-// While size of priority queue
-// is more than 1
-while (pq.length > 1) {
-// Extract shortest two ropes from pq
-let first = pq.shift();
-let second = pq.shift();
-
-// Connect the ropes: update result
-// and insert the new rope to pq
-res += first + second;
-pq.push(first + second);
-pq.sort(function(a,b){return a-b;});
-}
-return res;
-}
+	res.innerHTML=sum;
+  
+  
+}  
